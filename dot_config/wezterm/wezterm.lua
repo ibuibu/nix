943b15d68c10bd83524dc62b498ca490b595524a
ibuibu <ibuibu69@gmail.com>
2025-02-08 20:05:53 +0900

    add wezterm configs like tmux

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -8,6 +8,59 @@ config.font = wezterm.font_with_fallback({
 	"Noto Sans Mono CJK JP",
 })
 
+config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 2000 }
+local act = wezterm.action
+config.keys = {
+	{
+		key = "\\",
+		mods = "LEADER",
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "-",
+		mods = "LEADER",
+		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "z",
+		mods = "LEADER",
+		action = act.TogglePaneZoomState,
+	},
+	{
+		key = "h",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "l",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "k",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "j",
+		mods = "LEADER",
+		action = act.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "c",
+		mods = "LEADER",
+		action = act.SpawnTab("CurrentPaneDomain"),
+	},
+}
+
+for i = 1, 8 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = act.MoveTab(i - 1),
+	})
+end
+
 config.color_scheme = "Tokyo Night"
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
