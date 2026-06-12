c31366123edf019be36b81f9706f07f0e42e07df
ibuibu <ibuibu69@gmail.com>
2026-06-12 09:06:21 +0900

    feat: glow-preview.nvim に <leader>mp のショートカットを追加

--- chezmoi/dot_config/nvim/lua/plugins/glow-preview.lua
+++ chezmoi/dot_config/nvim/lua/plugins/glow-preview.lua
@@ -2,4 +2,7 @@ return {
   "ibuibu/glow-preview.nvim",
   cmd = "GlowPreview",
   ft = "markdown",
+  keys = {
+    { "<leader>mp", "<cmd>GlowPreview<cr>", ft = "markdown", desc = "Glow Preview" },
+  },
 }
