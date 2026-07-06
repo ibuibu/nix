097fd868d5d294450fe08dbdfcf5d941e6335745
ibuibu <ibuibu69@gmail.com>
2026-07-06 19:32:32 +0900

    fix: nvim の grep hidden 設定を fzf-lua に対して行う
    <leader>sg の picker は auto 判定で fzf-lua が使われており、snacks 側に
    grep.hidden = true を設定しても効かなかった。fzf-lua の grep opts に
    hidden = true を設定して --hidden を付与し、dotfiles を検索対象に含める。
    効かなかった snacks.lua は削除。

---
+++ chezmoi/dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,8 @@
+return {
+  "ibhagwan/fzf-lua",
+  opts = {
+    -- <leader>sg のライブ grep で hidden ファイル(dotfiles)も最初から対象にする
+    -- (<a-h> の toggle_hidden を最初からオンにした状態)
+    grep = { hidden = true },
+  },
+}
